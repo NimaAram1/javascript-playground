1eb2a460165a61bbe4947f6c475fc8f375e568d5
NimaAram <aramnima50@gmail.com>
2020-07-05 23:13:42 +0430

    part 2

--- site.js
+++ site.js
@@ -1,7 +1,22 @@
 // change header value
-
 var header = document.getElementById('web');
 var pre_header = document.getElementById('header_value');
+
 function changeHeader(){
+    
     header.innerHTML = pre_header.value
+    pre_header.value = null
+    
 }
+
+var detail = document.getElementById('web-details');
+var pre_detail = document.getElementById('header_value');
+
+
+
+    function chanegeDetails(){
+   
+        detail.innerHTML = pre_header.value
+        pre_header.value = null
+        
+    }
