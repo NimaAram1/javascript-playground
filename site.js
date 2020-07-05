ce33e2dec6f46489203e62fc18b7c651cc5d505d
NimaAram <aramnima50@gmail.com>
2020-07-05 23:59:04 +0430

    part 3

--- site.js
+++ site.js
@@ -20,3 +20,37 @@ var pre_detail = document.getElementById('header_value');
         pre_header.value = null
         
     }
+
+// -------------ES6 part---------------- //
+// var -> function
+// let -> block
+// const -> block -> read-only
+let y = 0;
+y = 1; 
+const x = 1;
+try{
+    x = 2;
+}catch(error){
+    console.log(error.message)
+}
+
+
+// objects
+
+
+
+const person = {
+    name : 'Nima',
+    walk : function(){},
+    talk : function(){},
+};
+const targetMember = 'name'
+console.log(person.walk())
+console.log(person[targetMember.value] = 'Ali')
+console.log(person.name)
+
+
+
+// this Keyword
+
+
