39d40d9241f6643322cb67d783c7c5b1daa01dd3
NimaAram <aramnima50@gmail.com>
2020-07-05 22:29:17 +0430

    change value of header

---
+++ site.js
@@ -0,0 +1,7 @@
+// change header value
+
+var header = document.getElementById('web');
+var pre_header = document.getElementById('header_value');
+function changeHeader(){
+    header.innerHTML = pre_header.value
+}
